f2f8ad4d257a0a00c26078bab99188d7958b368d
Marko Matic <mail.marko.matic@gmail.com>
2026-05-11 10:00:44 +0200

    - fix ads consent on android

--- src/display-consent/index.js
+++ src/display-consent/index.js
@@ -1,8 +1,9 @@
 import mobileAds, {
     AdsConsent
 } from 'react-native-google-mobile-ads';
+import { OS } from '@codexporer.io/expo-device';
 
-export const requestAdsDisplayConsent = async () => {
+const requestConsentDisplay = async () => {
     try {
         await AdsConsent.requestInfoUpdate();
         const adsConsentInfo = await AdsConsent.loadAndShowConsentFormIfRequired();
@@ -14,5 +15,17 @@ export const requestAdsDisplayConsent = async () => {
         console.log('Error during requesting ads permissions.');
         // eslint-disable-next-line no-console
         console.error(error);
+    }
+};
+
+export const requestAdsDisplayConsent = async () => {
+    if (!OS.isAndroid()) {
+        await requestConsentDisplay();
+    }
+};
+
+export const requestAdsDisplayConsentAndroid = async () => {
+    if (OS.isAndroid()) {
+        await requestConsentDisplay();
     }
 };
